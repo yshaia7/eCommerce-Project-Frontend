8c9fbc999dcf2869e44a1db43a2c221b0e1e2234
yshaia7 <yshaia7@gmail.com>
2022-07-13 20:51:50 +0300

    checkout form drop down list

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
     
   ],
   imports: [
--- angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,12 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {  Observable, of } from 'rxjs';
+import { Country } from '../common/country';
+import {map} from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Luv2ShopFormService {
 
-  constructor() { }
+  private contriesUrl = 'http://localhost:8080/api/countries';
+  private stateUrl =  'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.contriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]>{
+
+    // searchUrl
+    const searchStatesUrl  = `${this.stateUrl}/search/findByCountryCode?code=${theCountryCode}`;
+    
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
     
@@ -15,7 +38,7 @@ export class Luv2ShopFormService {
     // build an array for "Month" dropdown list
     // - start at current month and loop until
 
-    // Todo - change the number 12 to consts
+    // Todo - change the number 12 to const
     for (let theMonth = startMonth; theMonth <= 12; theMonth++){
       data.push(theMonth);
     }
@@ -38,5 +61,18 @@ export class Luv2ShopFormService {
     }
 
     return of(data)
+  }
+}
+
+// Todo move to diffrent file that contain all interfaces
+interface GetResponseCountries{
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates{
+  _embedded: {
+    states: State[];
   }
 }
